0cadcc218fb8a59e5e5d594a74cebae999892053
vidarc - Matt Ailes <mattailes@gmail.com>
2016-01-12 18:57:31 -0500

    some member stuff added

--- Member.cpp
+++ Member.cpp
@@ -1,1 +1,45 @@
 #include "Member.h"
+
+// default constructor -- does nothing
+Member::Member() {
+
+}
+
+// main constructor
+// take a pre-made ID number from Library class and 
+// create a Member object with that
+Member::Member(int id) {
+	m_id = id;
+
+	// temp variables to take in the info
+	std::string name;
+	std::string address;
+	std::string city;
+	std::string state;
+	int zipcode;
+
+	std::cout << "Enter the name of the member: ";
+	std::cin >> name;
+	m_name = name;
+
+	std::cout << "Enter the address of the member: ";
+	std::cin >> address;
+	m_address = address;
+
+	std::cout << "Enter the city: ";
+	std::cin >> city;
+	m_city = city;
+
+	std::cout << "Enter the state: ";
+	std::cin >> state;
+	m_state = state;
+
+	std::cout << "Enter the zipcode: ";
+	std::cin >> zipcode;
+	m_zipcode = zipcode;
+}
+
+// default deconstructor
+Member::~Member() {
+
+}
--- Member.h
+++ Member.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <iostream>
 #include <string>
 #include <vector>
 
@@ -16,5 +17,23 @@ private:
 	std::vector<Book> m_books;
 
 public:
+	Member();
+	Member(int);
+	~Member();
 
+	// Set functions
+	void setID(int id) { m_id = id; }
+	void setName(std::string name) { m_name = name; }
+	void setAddress(std::string address) { m_address = address; }
+	void setCity(std::string city) { m_city = city; }
+	void setState(std::string state) { m_state = state; }
+	void setZipcode(int zipcode) { m_zipcode = zipcode; }
+
+	// Get functions
+	int getID() { return m_id; }
+	std::string getName() { return m_name; }
+	std::string getAddress() { return m_address; }
+	std::string getCity() { return m_city; }
+	std::string getState() { return m_state; }
+	int getZipcode() { return m_zipcode; }
 };
